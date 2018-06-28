0b4257d540654debbd1985f909e9f55adb5e5890
Gleb Kulik <mr.kulik23@gmail.com>
2018-06-28 13:24:49 +0300

    Init

---
+++ BTC/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  BTC
+//
+//  Created by Gleb Kulik on 6/28/18.
+//  Copyright © 2018 Sandpiper Software. All rights reserved.
+//
+
+import Foundation
+
+class Const {
+    static let URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+}
--- BTC/ViewController.swift
+++ BTC/ViewController.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var btcValueLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let currencyType = ["EUR","USD","RUB"]
+    
     override func viewDidLoad() {
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyType.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyType[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
 }
 
